035a386b15cd68f438b3ce9dc0d98a59ad3ef3b0
benjamin_brady <130180703+wizardmonkey83@users.noreply.github.com>
2024-11-07 16:48:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
-## inspiration taken from various sources covering this well known example. 
-## however, code is edited to add simplicity and comments added to show understanding of the core concepts regarding k-means clustering
+## clustering planatary systems based on:
+## Stellar Effective Temperature [K]
+## Stellar Radius [Solar Radius]
+## Stellar Mass [Solar mass]
+## Stellar Metallicity [dex]
+## Planet Radius [Jupiter Radius / Earth Radius]
+## Planet Mass [Earth Mass / Jupiter Mass]
+## Eccentricity
+## Number of Planets
+## Discovery Year
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.cluster import KMeans
 
-## transfers the .csv dataset into a pandas dataframe
-df = pd.read_csv("/your/path/to/Mall_Customers.csv")
 
+## loads the .csv file to a pandas dataframe
+df = pd.read_csv('/content/planatary_clustering.csv', sep=',', on_bad_lines='skip')
 
-## isolates the values that will be  tested --> customers annual income and spending score
-x = df.iloc[:, [3, 4]].values
+## identifies the values that the model will track
+NUMERIC_COLUMNS = ['st_teff', 'st_rad', 'st_mass', 'st_met', 'pl_radj', 'pl_bmassj', 'pl_orbeccen', 'sy_pnum', 'disc_year']
 
-## creates a list (wcss) that will store the cluster values 1-10
-## creates a 'KMeans' object from the sklearn.cluster module (kmeans = KMeans()). 'n_clusters=i' sets the number of clusters to value 'i'. "init='k-means++'" sets the clusters farther apart, ensuring that as many datapoints as possible are covered, discouraging competition between centroids. 'random_state=42' directs the model to start with the same amount of random numbers each time it runs. 
-## 'for i in range(1,11)' loops through number of clusters 'i' 10 times.
+## loops through each value in 'NUMERIC_COLUMNS'
+for column in NUMERIC_COLUMNS:
+    ## converts all values to numeric values, forcing any non-numerical values to NaN
+    df[column] = pd.to_numeric(df[column], errors='coerce')
+
+## since this dataset isn't perfect, a lot of values are missing, this drops the missing values. another route could be taking the mean of all values in a column and replacing a missing value with the columns mean. 
+df.dropna(subset=NUMERIC_COLUMNS, inplace=True)
+
+## 'x' is defined by the numerical values of names in 'NUMERICAL_COLUMNS'. this extracts the values that will be used for clustering
+x = df[NUMERIC_COLUMNS].values
+
+## this is necessary for finding the optimal cluster count
+## uses matplotlib to create a plot, with x-axis = number of clusters 10, the y-axis holding wcss values, and "marker='o'" making each datapoint a circle
 wcss = [KMeans(n_clusters=i, init='k-means++', random_state=42).fit(x).inertia_ for i in range(1, 11)]
 
+## didn't feel the need to add in the code for graphinh. optimal cluster size is 3
+
 ## creates a 'KMeans' object from the sklearn.cluster module (kmeans = KMeans()). sets the number of clusters = 5. 
-kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+kmeans = KMeans(n_clusters=3, init='k-means++', random_state=42)
 ## 'kmeans.fit_predict(x)' trains the model on the values in variable 'x', finds the optimal locations for cluster centers based on the datapoints. then it assigns each datapoint to a cluster. 
 y_kmeans = kmeans.fit_predict(x)
 
-## sets the size of the graph to 8 by 8, making it square
-plt.figure(figsize=(8, 8))
-## defines the colors used for datapoints
-colors = ['red', 'blue', 'green', 'cyan', 'magenta']
-## loops through the number of clusters set previously
-for i in range(5):
-    ## 'plt.scatter' creates a scatterplot of datapoints for each cluster
-    ## 'x[y_kmeans == i, 0]' selects all datapoints that correspond to the value of i, then x-coordinates of the datapoints is extracted
-    ## 'x[y_kmeans == i, 1]' selects 'y' coordinates for datapoints in cluster 'i'
-    ## 's=100' sets the datapoint size
-    ## 'c=colors[i]' sets the color equal to its corresponding cluster value
-    ## "label=f'Cluster {i+1}'" provides a label for the clusters in the graph
-    plt.scatter(x[y_kmeans == i, 0], x[y_kmeans == i, 1], s=100, c=colors[i], label=f'Cluster {i+1}')
+## adds the cluster labels to the dataframe for visualization
+df['Cluster'] = y_kmeans
 
-## plots the centroids in the center of the clusters
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
-plt.legend()
+## colors that will represent the clusters
+colors = ['pink', 'teal', 'orange']
+
+## generates a pair plot for for the clusters to determine which relations are best for clustering/finding differences
+sns.pairplot(df[NUMERIC_COLUMNS + ['Cluster']], hue='Cluster', palette=colors[:3])
 plt.show()
